42d9a7396bd3a9891e2492a6029b239eadb1e5a4
Asmit <bhantanasmit@gmail.com>
2020-04-25 19:06:13 +0545

    added some user testing and super user
    overrided the function for creating the super user

--- recipe/core/tests/test_models.py
+++ recipe/core/tests/test_models.py
@@ -11,8 +11,26 @@ class ModelTests(TestCase):
         password= 'Testpass123'
         user = get_user_model().objects.create_user(
             email = email,
-            password = password
+            password = password,
         )
         self.assertEqual(user.email,email)
         self.assertTrue(user.check_password(password))
+    def test_new_user_email_normalaized(self):
+        '''''Test the email for a new user is noramlized'''
+        email = 'test@FAFD.com'
+        user = get_user_model().objects.create_user(email,'test123')
+        self.assertEqual(user.email,email.lower())
+        
+    def test_new_user_invalid_email(self):
+        ''''Test creating the user with no email raise error'''
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,'test123')
+    def test_create_new_superuser(self):
+        '''Test creating new superuser'''
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
         
