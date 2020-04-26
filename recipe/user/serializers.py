876a5386edad2aac24d25dffe21065bc0581ffa8
Asmit <bhantanasmit@gmail.com>
2020-04-26 13:26:16 +0545

    added api endpoints for creating and authenticcacte users

---
+++ recipe/user/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model,authenticate
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    '''Serializer for the user object'''
+    class Meta:
+        model = get_user_model()
+        fields = ('email','password','name')
+        '''Setting extra arguments for field variable'''
+        extra_kwargs = {'password':{'write_only':True,'min_length':5}}
+
+    def create(self,validated_data):
+        '''Create new user with encrypted data and returns it'''
+        return get_user_model().objects.create_user(**validated_data)
+
+class AuthTokenSerializer(serializers.Serializer):
+    '''Serializer for user auth object'''
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type':'password'},
+        trim_whitespace=False #don't remove the whitespaces
+    )
+
+    def validate(self,attrs):
+        '''Validate and auth user'''
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            userName = email,
+            password=password
+        )
+        if not user:
+            msg = {'Unable to authenticate with provided credentials'}
+            raise serializers.ValidationError(msg,code='authentication')
+
+        attrs['user']=user
+        return attrs
