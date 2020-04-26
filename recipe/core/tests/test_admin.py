e3754176fc23d82e3f468eec3860693a1a6ac332
Asmit <bhantanasmit@gmail.com>
2020-04-26 12:10:00 +0545

    customized admin users

--- recipe/core/tests/test_admin.py
+++ recipe/core/tests/test_admin.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 class AdminSiteTests(TestCase):
@@ -22,3 +23,15 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res,self.user.name)
         self.assertContains(res,self.user.email)
+    def test_user_change_page(self):
+        '''Test that use edit page works'''
+        url = reverse('admin:core_user_change',args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code,200)
+    def test_create_user_page(self):
+        '''Test the create user page works'''
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code,200)
+        
