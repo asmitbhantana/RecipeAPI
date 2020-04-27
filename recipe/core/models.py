9e7a82bce617f8145f206575afbc1c4a74fb8fdc
Asmit <bhantanasmit@gmail.com>
2020-04-27 13:39:43 +0545

    added image field to recipe model

--- recipe/core/models.py
+++ recipe/core/models.py
@@ -1,8 +1,17 @@
+import uuid
+import os
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,\
     PermissionsMixin
 from django.conf import settings
 # Create your models here.
+def recipe_image_file_path(instance,filename):
+    '''Generate the file path for new recipe image'''
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join('uploads/recipe/',filename)
+
 class UserManager(BaseUserManager):
 
     def create_user(self,email,password=None,**extra_fileds):
@@ -69,6 +78,7 @@ class Recipe(models.Model):
     time_minutes  = models.IntegerField()
     price = models.DecimalField(max_digits=5,decimal_places=2)
     link = models.CharField(max_length=255,blank=True)
+    image = models.ImageField(null=True,upload_to=recipe_image_file_path)
     """django have the features of providing the name of the field in the quotes
     that is mapped in to the model as required"""
     ingredients=models.ManyToManyField('Ingredient')
